afc0f885657900e5cb12c669d4b395bd672c1746
Felipe Cardozo <fraifelipe@gmail.com>
2018-04-15 01:47:17 -0300

    Aula 3

---
+++ client/app/cadastro/cadastro.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FotoService } from '../foto/foto.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'cadastro',
+    templateUrl: './cadastro.component.html' 
+})
+export class CadastroComponent { 
+
+    foto: FotoComponent = new FotoComponent();
+    meuForm: FormGroup;
+    service: FotoService;
+
+    constructor(service: FotoService, fb: FormBuilder) {
+        
+        this.service = service;
+
+        this.meuForm = fb.group({
+            titulo: ['', Validators.compose(
+                [Validators.required, Validators.minLength(4)]
+            )],
+            url: ['', Validators.required],
+            descricao: [''],
+        });
+    }
+
+    cadastrar(event) {
+        event.preventDefault();
+
+        console.log(this.foto);
+
+        this.service
+            .cadastra(this.foto)
+            .subscribe(() => {
+                console.log("Foto cadastrada com sucesso");
+                this.foto = new FotoComponent();
+            }, erro => console.log(erro));
+    }
+    
+}
---
+++ client/app/listagem/listagem.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FotoService } from  '../foto/foto.service'
+import { FotoComponent } from '../foto/foto.component';
+
+@Component({
+    moduleId: module.id,
+    selector: 'listagem',
+    templateUrl: './listagem.component.html' 
+})
+export class ListagemComponent { 
+
+    fotos: FotoComponent[] = [];
+    service: FotoService;
+
+    constructor(service: FotoService) {
+        this.service = service
+        this.service
+            .lista()
+            .subscribe(fotos => {
+                this.fotos = fotos;
+            }, erro => console.log(erro));
+    }
+    remove(foto){
+        this.service
+            .remove(foto)
+            .subscribe(
+                () =>{
+
+                    console.log('Foto removida com sucesso')
+                    let novasFotos = this.fotos.slice(0);
+                    let indice = novasFotos.indexOf(foto);
+                    novasFotos.splice(indice, 1);
+                    this.fotos = novasFotos;
+                
+            },
+            erro => console.log(erro)
+        );
+    }
+
+}
